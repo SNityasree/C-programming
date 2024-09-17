3b6317f24976c3fd87c12f8023638d8c061cf4ac
Nityasree <159861640+SNityasree@users.noreply.github.com>
2024-09-17 11:53:51 +0530

    Update pyramid.c

--- pattern.c/star pattern/pyramid.c
+++ pattern.c/star pattern/pyramid.c
@@ -1,1 +1,16 @@
-void
+#include<stdio.h>
+int main(){
+    int n,i,j,sp;
+    scanf("%d",&n);
+    int n1=n;
+    for(i=0;i<n*2;i=i+2){
+        for(sp=n1;sp>0;sp--)
+            printf("  ");
+            for(j=0;j<=i;++j)
+        {
+         printf("* ");   
+        }
+        printf("\n");
+        n1--;
+    }
+}
